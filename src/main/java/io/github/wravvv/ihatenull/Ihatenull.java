f9a113b122d8d0d9201c36ca4c28b109f168a662
Joe Bob <jobtangii@outlook.com>
2025-03-31 14:28:10 +0800

    Version 4.0.1 for 1.19.2

--- src/main/java/io/github/wravvv/ihatenull/Ihatenull.java
+++ src/main/java/io/github/wravvv/ihatenull/Ihatenull.java
@@ -1,11 +1,7 @@
 package io.github.wravvv.ihatenull;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -14,22 +10,12 @@ public class Ihatenull {
 
     // Define mod id in a common place for everything to reference
     public static final String MODID = "ihatenull";
+    public static final String MODNAME = "IHateNull";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public Ihatenull() {
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("IHateNull >> Client Setup");
-        }
-    }
 }
--- src/main/java/io/github/wravvv/ihatenull/mixin/MixinMissingTextureAtlasSprite.java
+++ src/main/java/io/github/wravvv/ihatenull/mixin/MixinMissingTextureAtlasSprite.java
@@ -2,27 +2,32 @@ package io.github.wravvv.ihatenull.mixin;
 
 import com.mojang.blaze3d.platform.NativeImage;
 import io.github.wravvv.ihatenull.Ihatenull;
-import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.*;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static io.github.wravvv.ihatenull.Ihatenull.LOGGER;
+import static io.github.wravvv.ihatenull.Ihatenull.*;
 
 @Mixin(MissingTextureAtlasSprite.class)
-public class MixinMissingTextureAtlasSprite {
+public class MixinMissingTextureAtlasSprite{
 
-    @Inject(at=@At(value="HEAD"),method= "generateMissingImage(II)Lcom/mojang/blaze3d/platform/NativeImage;",cancellable = true)
-    private static void generateMissingImage(int p_249811_, int p_249362_, CallbackInfoReturnable<NativeImage> cir){
-        cir.cancel();
-
+    @Shadow
+    private static final ResourceLocation MISSING_TEXTURE_LOCATION = new ResourceLocation("missingno");
+    @Shadow
+    private static final LazyLoadedValue<NativeImage> MISSING_IMAGE_DATA = new LazyLoadedValue(() -> {
         BufferedImage nullTexture = null;
         new File("config/ihatenull").mkdirs();
         File textureFile = new File("config/ihatenull/null.png");
@@ -31,34 +36,35 @@ public class MixinMissingTextureAtlasSprite {
 
                 try (InputStream configStream = Ihatenull.class.getResourceAsStream("/null.png")){
                     if (configStream != null) {
-                        LOGGER.info("IHateNull >> Default null.png taken from: /null.png");
+                        LOGGER.info("{} >> Default null.png taken from: /null.png", MODNAME);
                         ImageIO.write(ImageIO.read(configStream),"png",textureFile);
                     } else {
-                        LOGGER.info("IHateNull >> Default null.png Resource not found");
+                        LOGGER.info("{} >> Default null.png Resource not found", MODNAME);
                     }
                 } catch (Exception e) {
-                    LOGGER.error("IHateNull >> Error setting default");
+                    LOGGER.error("{} >> Error setting default", MODNAME);
                     LOGGER.error(e.getMessage());
                 }
 
             }
         } catch (IOException e) {
-            LOGGER.error("IHateNull >> Error setting up config");
+            LOGGER.error("{} >> Error setting up config", MODNAME);
             LOGGER.error(e.getMessage());
         }
         try {
             nullTexture = ImageIO.read(textureFile);
         } catch (Exception e) {
-            LOGGER.error("IHateNull >> Error loading texture");
+            LOGGER.error("{} >> Error loading texture", MODNAME);
             LOGGER.error(e.getMessage());
         }
-        LOGGER.info("IHateNull >> Texture empty? " + (nullTexture==null));
+        LOGGER.info("{} >> Texture empty? {}", MODNAME, (nullTexture == null));
 
-        NativeImage missingTexture;
+
+        NativeImage nativeImage;
         if (nullTexture == null){
-            missingTexture = new NativeImage(p_249811_, p_249362_, false);
+            nativeImage = new NativeImage(16, 16, false);
         } else {
-            missingTexture = new NativeImage(nullTexture.getWidth(), nullTexture.getHeight(), false);
+            nativeImage = new NativeImage(nullTexture.getWidth(), nullTexture.getHeight(), false);
         }
 
         for(int i = 0; i < 16; ++i) {
@@ -68,17 +74,35 @@ public class MixinMissingTextureAtlasSprite {
 
                 if (nullTexture == null){
                     if (i < 8 ^ j < 8) {
-                        missingTexture.setPixelRGBA(j, i, -1);
+                        nativeImage.setPixelRGBA(j, i, -1);
                     } else {
-                        missingTexture.setPixelRGBA(j, i, -16777216);
+                        nativeImage.setPixelRGBA(j, i, -16777216);
                     }
                 } else {
                     int argb = nullTexture.getRGB(j,i);
-                    missingTexture.setPixelRGBA(j, i, (argb & 0xFF00FF00) | ((argb & 0x000000FF) << 16) | ((argb & 0x00FF0000) >>> 16));
+                    nativeImage.setPixelRGBA(j, i, (argb & 0xFF00FF00) | ((argb & 0x000000FF) << 16) | ((argb & 0x00FF0000) >>> 16));
                 }
             }
         }
 
+        nativeImage.untrack();
+        return nativeImage;
+    });
+
+    @Inject(at=@At(value="HEAD"), method= "getTexture()Lnet/minecraft/client/renderer/texture/DynamicTexture;", cancellable = true)
+    private static void getTexture(CallbackInfoReturnable<DynamicTexture> cir){
+        cir.cancel();
+
+        if (missingTexture == null) {
+            missingTexture = new DynamicTexture(MISSING_IMAGE_DATA.get());
+            Minecraft.getInstance().getTextureManager().register(MISSING_TEXTURE_LOCATION, missingTexture);
+        }
+
         cir.setReturnValue(missingTexture);
     }
+
+    @Shadow
+    @Nullable
+    private static DynamicTexture missingTexture;
+
 }
