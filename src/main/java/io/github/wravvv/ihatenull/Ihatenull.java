a4dd933fca2c0f0166aea5a5dced51e2c4d4bc6d
Wrav <wravv@outlook.com>
2025-07-23 23:34:39 +0800

    neoforge 1.21.1 6.0.3

--- src/main/java/io/github/wravvv/ihatenull/Ihatenull.java
+++ src/main/java/io/github/wravvv/ihatenull/Ihatenull.java
@@ -1,10 +1,9 @@
 package io.github.wravvv.ihatenull;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Ihatenull.MODID)
 public class Ihatenull {
 
